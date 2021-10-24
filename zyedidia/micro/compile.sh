51d35e71505b0af3f2eaf722e81c6294cf7ca66b
R2boyo25 <r2boyo25@gmail.com>
2021-10-24 15:20:19 -0500

    aaaaaaaaaaaaaaaaaa

--- zyedidia/micro/compile.sh
+++ zyedidia/micro/compile.sh
@@ -1,3 +1,13 @@
+if go
+then
+    :
+else
+    echo "Go is not installed!"
+    echo "please follow the guide at https://golang.org/doc/install"
+    echo "I attempted to get avalon to compile and install go but it just segfaults, sorry"
+    exit
+fi
+
 if make build
 then
     :
