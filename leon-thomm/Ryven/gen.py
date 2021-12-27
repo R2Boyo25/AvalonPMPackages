24f33483cc114d177c2ba5d5bb97443009073d8d
R2boyo25 <r2boyo25@gmail.com>
2021-12-27 15:54:35 -0600

    Fix ryven

--- leon-thomm/Ryven/gen.py
+++ leon-thomm/Ryven/gen.py
@@ -3,5 +3,5 @@ import sys
 binf = sys.argv[1]
 filesFolder = sys.argv[2]
 
-with open('ryven', 'w') as f:
-    f.write(f'#!/bin/bash\npython3 "{filesFolder}/Ryven/src/Ryven.py" "$@"')
+with open('ryven-runner', 'w') as f:
+    f.write(f'#!/bin/bash\npython3 "{filesFolder}/ryven/main/Ryven.py" "$@"')
