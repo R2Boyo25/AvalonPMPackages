d7f7c48ad6e28869bbf501c4a6d4b6978283ef7c
R2boyo25 <r2boyo25@gmail.com>
2022-02-13 16:03:03 -0600

    a

--- Kindrat/cassandra-client/gen.py
+++ Kindrat/cassandra-client/gen.py
@@ -4,4 +4,4 @@ binf = sys.argv[1]
 filesFolder = sys.argv[2]
 
 with open(binf, 'w') as f:
-    f.write(f'#!/bin/bash\njava -jar "{filesFolder}/build/libs/cassadra-client-1.0.2-exec.jar" "$@"')
+    f.write(f'#!/bin/bash\njava -jar "{filesFolder}/build/libs/cassadra-client-0.0.0-0-exec.jar" "$@"')
